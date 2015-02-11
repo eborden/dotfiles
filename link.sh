9450a7e9a9e48370a3b1bd3322e2d712c8ceb5cc
Evan Rutledge Borden <evan.borden@skedge.me>
2015-02-11 12:37:26 -0500

    initial

---
+++ link.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu
+
+basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+mkdir -p ~/.xmonad
+ln -s $basedir/xmonad.hs ~/.xmonad/
+ln -s $basedir/.xmobarrc ~/
+ln -s $basedir/.xprofile ~/
+ln -s $basedir/.Xdefaults ~/
---
+++ xmonad.hs
@@ -0,0 +1,55 @@
+import XMonad
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.FadeInactive
+import XMonad.Actions.CycleWS
+import System.IO
+import qualified Data.Map as Map
+
+myModMask = mod4Mask
+lightBlue = "#073642"
+orange = "#cb4b16"
+red = "#dc322f"
+
+main = do
+  xproc <- spawnPipe "/home/eborden/.nix-profile/bin/xmobar /home/eborden/.xmobarrc"
+  xmonad $ withUrgencyHook NoUrgencyHook defaultConfig
+    { manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , modMask = myModMask
+    , logHook = do
+        fadeInactiveLogHook 0.85
+        dynamicLogWithPP xmobarPP
+          { ppOutput = hPutStrLn xproc
+          , ppTitle = xmobarColor orange "" . shorten 100
+          , ppUrgent = xmobarColor lightBlue orange . wrap ">" "<"
+          }
+    , focusFollowsMouse = False
+    , keys = newKeys $ keys defaultConfig
+    , borderWidth = 0
+    , terminal = "terminator"
+    }
+
+xK_fnLeft = 0x1008ff11
+xK_fnRight = 0x1008ff13
+xK_fnDown = 0x1008ff03
+xK_fnUp = 0x1008ff02
+
+newKeys :: (XConfig Layout -> Map.Map (ButtonMask, KeySym) (X ()))
+        -> (XConfig Layout -> Map.Map (ButtonMask, KeySym) (X ()))
+newKeys x = \y -> Map.union (Map.fromList
+  [ ((myModMask, xK_p), (spawn "dmenu_run  -fn \"-adobe-helvetica-medium-r-*-*-30-*-*-*-*-*-*-*\""))
+  , ((0, xK_fnRight), (spawn "amixer sset Master 1%+"))
+  , ((0, xK_fnLeft), (spawn "amixer sset Master 1%-"))
+  , ((myModMask, xK_Down), (spawn "amixer sset Master toggle"))
+  , ((controlMask .|. myModMask, xK_Left), (prevWS))
+  , ((controlMask .|. myModMask, xK_Right), (nextWS))
+  , ((controlMask .|. myModMask, xK_h), (prevWS))
+  , ((controlMask .|. myModMask, xK_l), (nextWS))
+  --, ((controlMask .|. myModMask, xK_Up), (XMonad.workspaces 1))
+  --, ((controlMask .|. myModMask, xK_Down), (XMonad.workspaces 9))
+  , ((0, xK_fnUp), spawn "xbacklight +10")
+  , ((0, xK_fnDown), spawn "xbacklight -10")
+  ]) (x y)
